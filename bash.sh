53f6ab7e52aff89d0055a4143ffe6d24a1ea739a
Alejo Grigera Sutro <23289347+artofthesmart@users.noreply.github.com>
2026-04-10 14:28:56 -0700

    Rename sources list file for GitHub CLI

--- bash.sh
+++ bash.sh
@@ -72,7 +72,7 @@ if ! command -v gh &> /dev/null; then
   wget -nv -O "$GH_KEYRING_TMP" https://cli.github.com/packages/githubcli-archive-keyring.gpg
   cat "$GH_KEYRING_TMP" | $SUDO tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
   $SUDO chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | $SUDO tee /etc/apt/sources.list.d/github-cli.sources > /dev/null
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | $SUDO tee /etc/apt/sources.list.d/github-cli.list > /dev/null
   $SUDO apt-get update -y
   $SUDO apt-get install gh -y
   rm -f "$GH_KEYRING_TMP"
